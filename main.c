09e9ba92b781be19d99b57f60a6b0c89c52fc0cf
Anthony Maton <maitre.negi@gmail.com>
2016-04-25 15:43:18 +0200

    Add first basic code

---
+++ main.c
@@ -0,0 +1,34 @@
+/*
+ * Série 3
+ * Arszagi vel Harszagi Kamil : karszag15
+ * Maton Anthony : amaton15
+ *
+ * Point d'entrée de l'application serveur et boucle principale du serveur.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/ipc.h>
+#include <sys/socket.h>
+#include <sys/shm.h>
+#include <signal.h>
+#include <errno.h>
+#include <string.h>
+
+void argument_check(int argc, char ** argv);
+
+int main(int argc, char ** argv){
+    argument_check(argc, argv);
+}
+
+void argument_check(int argc, char ** argv){
+    if(argc > 3 || argc < 2){
+        fprintf(stderr, "Usage incorrect : \n\t "
+                "Requiert au moins 1 argument : numéro de port\n\n"
+                "Argument optionnel : fichier de sortie des messages\n\n"
+                "USAGE : ./server PORT [Sortie]");
+        exit(EXIT_FAILURE);
+    }
+    if(argv[1])
+}
