9eb3a1c68f182d347fc9bbe087b690d0b355c987
Anthony Maton <maitre.negi@gmail.com>
2016-05-03 18:10:22 +0200

    Fix concurrent variable name

---
+++ client.c
@@ -0,0 +1,5 @@
+//
+// Created by Anthony Maton on 3/05/16.
+//
+
+#include "client.h"
---
+++ client.h
@@ -0,0 +1,8 @@
+//
+// Created by Anthony Maton on 3/05/16.
+//
+
+#ifndef BATAILLE_CLIENT_H
+#define BATAILLE_CLIENT_H
+
+#endif //BATAILLE_CLIENT_H
